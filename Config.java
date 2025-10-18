521745237fb13bc1663a9582385ebb1bd791db91
heisenstack <dinaniamine@gmail.com>
2025-10-18 16:38:20 +0100

    CONFIG

--- Config.java
+++ Config.java
@@ -1,3 +1,8 @@
 public class Config {
     private Config() {}
+
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 600;
+    public static final double ROAD_WIDTH = 100.0;
+    public static final double INTERSECTION_SIZE = ROAD_WIDTH / 2.0;
 }
