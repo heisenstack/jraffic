f7009302a74baa422197f643efd2a79c309e709a
heisenstack <dinaniamine@gmail.com>
2025-10-18 16:38:42 +0100

    CONFIG

--- Config.java
+++ Config.java
@@ -5,4 +5,7 @@ public class Config {
     public static final int WINDOW_HEIGHT = 600;
     public static final double ROAD_WIDTH = 100.0;
     public static final double INTERSECTION_SIZE = ROAD_WIDTH / 2.0;
+
+    public static final int TIMER_DELAY_MS = 16; 
+    public static final double MIN_SPAWN_DELAY_SEC = 0.3;
 }
