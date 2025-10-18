2f844e82d071cb4e6f29cb4825eeba813f073eca
heisenstack <dinaniamine@gmail.com>
2025-10-18 16:38:58 +0100

    CONFIG

--- Config.java
+++ Config.java
@@ -8,4 +8,8 @@ public class Config {
 
     public static final int TIMER_DELAY_MS = 16; 
     public static final double MIN_SPAWN_DELAY_SEC = 0.3;
+
+    public static final double MIN_GAP = INTERSECTION_SIZE * 1.75;
+    public static final double CAR_SPEED = 2.5;
+    public static final double CAR_SCALE_FACTOR = 0.8;
 }
