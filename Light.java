06464a459d2972925be275a213390b2ce2d90bac
heisenstack <dinaniamine@gmail.com>
2025-10-18 16:16:12 +0100

    LIGHT ADDING

--- Light.java
+++ Light.java
@@ -2,5 +2,10 @@ public class Light {
     double x, y, dirX, dirY;
     boolean isGreen = false;
 
-
+    public Light(double x, double y, double dX, double dY) {
+        this.x = x;
+        this.y = y;
+        this.dirX = dX;
+        this.dirY = dY;
+    }
 }
