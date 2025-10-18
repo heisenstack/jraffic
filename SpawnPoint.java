7d61cd88b19aeca5af388b224cd501be13a9f952
heisenstack <dinaniamine@gmail.com>
2025-10-18 16:33:30 +0100

    SPAWM POINT CONSTRUCTOR

--- SpawnPoint.java
+++ SpawnPoint.java
@@ -1,3 +1,9 @@
 public class SpawnPoint {
     double x, y, dirX, dirY;
+    public SpawnPoint(double x, double y, double dX, double dY) {
+        this.x = x;
+        this.y = y;
+        this.dirX = dX;
+        this.dirY = dY;
+    }
 }
