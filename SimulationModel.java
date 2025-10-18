6ede3d3932ef1c0ae50229aa7272d58060a290e5
heisenstack <dinaniamine@gmail.com>
2025-10-18 16:42:44 +0100

    SimulationModel Helpers

--- SimulationModel.java
+++ SimulationModel.java
@@ -21,4 +21,20 @@ public class SimulationModel {
         simulationStartTime = System.nanoTime();
         lastSwitchTime = System.nanoTime();
     }
+
+    private void initializeSimulation() {
+        Point center = new Point(Config.WINDOW_WIDTH / 2, Config.WINDOW_HEIGHT / 2);
+
+        // SpawnPoints
+        spawnPoints.add(new SpawnPoint(center.x - Config.INTERSECTION_SIZE, 0, 0, Config.CAR_SPEED));
+        spawnPoints.add(new SpawnPoint(center.x, Config.WINDOW_HEIGHT - Config.INTERSECTION_SIZE, 0, -Config.CAR_SPEED));
+        spawnPoints.add(new SpawnPoint(0, center.y, Config.CAR_SPEED, 0));
+        spawnPoints.add(new SpawnPoint(Config.WINDOW_WIDTH - Config.INTERSECTION_SIZE, center.y - Config.INTERSECTION_SIZE, -Config.CAR_SPEED, 0));
+
+        // Lights
+        lights.add(new Light(center.x - 2 * Config.INTERSECTION_SIZE, center.y - 2 * Config.INTERSECTION_SIZE, 0, Config.CAR_SPEED));
+        lights.add(new Light(center.x + Config.INTERSECTION_SIZE, center.y - 2 * Config.INTERSECTION_SIZE, -Config.CAR_SPEED, 0));
+        lights.add(new Light(center.x - 2 * Config.INTERSECTION_SIZE, center.y + Config.INTERSECTION_SIZE, Config.CAR_SPEED, 0));
+        lights.add(new Light(center.x + Config.INTERSECTION_SIZE, center.y + Config.INTERSECTION_SIZE, 0, -Config.CAR_SPEED));
+    }
 }
