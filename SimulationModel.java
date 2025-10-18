123841fb63aa4ea1d5d06f0f7bbe1a58d1a1299d
heisenstack <dinaniamine@gmail.com>
2025-10-18 16:41:26 +0100

    SimulationModel Properties

--- SimulationModel.java
+++ SimulationModel.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 public class SimulationModel {
-    
+    private final List<Car> cars = new ArrayList<>();
+    private final List<Light> lights = new ArrayList<>();
+    private final List<SpawnPoint> spawnPoints = new ArrayList<>();
+    private final HashMap<Integer, Long> lastSpawnTime = new HashMap<>();
+    private final Random random = new Random();
 }
