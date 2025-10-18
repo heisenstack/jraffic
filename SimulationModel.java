06469a31ee33fb8c2b0f7bf0363a6c2ad49de0fd
heisenstack <dinaniamine@gmail.com>
2025-10-18 16:43:33 +0100

    SimulationModel Helpers

--- SimulationModel.java
+++ SimulationModel.java
@@ -56,4 +56,7 @@ public class SimulationModel {
             totalCarsSpawned++;
         }
     }
+    public void trySpawnRandomCar() {
+        trySpawnCar(random.nextInt(spawnPoints.size()));
+    }
 }
