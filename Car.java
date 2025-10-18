bb25e91bb0d77983161d96ab85783bf2a807fd31
heisenstack <dinaniamine@gmail.com>
2025-10-18 16:47:15 +0100

    Car.java

--- Car.java
+++ Car.java
@@ -1,9 +1,11 @@
 import java.awt.Color;
+
 public class Car {
     double x, y, dirX, dirY;
     Turns turn;
     boolean turned = false;
     Color color;
+
     public Car(double x, double y, double dX, double dY, Turns t) {
         this.x = x;
         this.y = y;
@@ -11,5 +13,15 @@ public class Car {
         this.dirY = dY;
         this.turn = t;
         this.color = getRandomColor();
+    }
+
+    // Assigns color based on turn for easy debugging/viewing
+    private Color getRandomColor() {
+        switch (turn) {
+            case FORWARD: return new Color(30, 144, 255); // Blue
+            case LEFT:    return Color.ORANGE;
+            case RIGHT:   return new Color(153, 50, 204); // Purple
+            default:      return Color.WHITE;
+        }
     }
 }
